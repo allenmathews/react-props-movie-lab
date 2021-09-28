7016775f03bb93048f4062695aeade929e40befb
Allen Mathews <allenjoemathews@gmail.com>
2021-09-28 20:42:07 +1000

    commit tree 61e15d0b5a07a464ad67c29cfe5d56b3ee26a37e

--- src/card-components/CardBack.js
+++ src/card-components/CardBack.js
@@ -12,6 +12,12 @@ export default class CardBack extends Component {
 
   generateRatingElement = () => {
     // implement meeeee! See the readme for instructions
+    if(this.props.IMDBRating !== null) {
+      return <img src={imgMapper[this.props.IMDBRating]} alt="" />
+    }
+    else {
+      return <h4>No Rating Found</h4>
+    }
   }
 
   render() {
@@ -19,7 +25,7 @@ export default class CardBack extends Component {
       <div className="card-back">
         <h3 className="title"></h3>
         <span />
-        { /* your rating element should go here -- you can invoke methods within JSX, à la: this.myMethod() */ }
+        { this.generateRatingElement() }
         <span />
         <h5 className="genres"></h5>
       </div>
